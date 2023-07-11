15bc43ab3eacbae20f87fdf0177553a89eabda45
Hiro Kato <hirokikato1@gmail.com>
2023-07-11 17:55:15 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(event='roller-skate'){
+    return `This Saturday, I want to ${event}!`
+}
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flairString="*"){
+ return function(inParam="special"){
+    return `You are ${flairString}${inParam}${flairString}!`
+ }   
+}
